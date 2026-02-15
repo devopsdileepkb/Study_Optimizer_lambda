167b40643c2114737fa1892f745fdcf9e9200f82
devopsdileepkb <dilee8217@gmail.com>
2026-02-15 19:23:21 +0530

    ...

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,7 +1,9 @@
-output "lambda_function_name" {
-  value = aws_lambda_function.zip_csv_lambda.function_name
+output "lambda_name" {
+  description = "Name of the Lambda function"
+  value       = aws_lambda_function.zip_csv_lambda.function_name
 }
 
-output "lambda_function_arn" {
-  value = aws_lambda_function.zip_csv_lambda.arn
+output "lambda_arn" {
+  description = "ARN of the Lambda function"
+  value       = aws_lambda_function.zip_csv_lambda.arn
 }
