2f70fe3322d2f5531fbb591b91577e0fb75c77df
devopsdileepkb <dilee8217@gmail.com>
2026-02-15 19:25:08 +0530

    11

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,9 +1,7 @@
-output "lambda_name" {
-  description = "Name of the Lambda function"
-  value       = aws_lambda_function.zip_csv_lambda.function_name
+output "lambda_function_name" {
+  value = module.lambda.lambda_name
 }
 
-output "lambda_arn" {
-  description = "ARN of the Lambda function"
-  value       = aws_lambda_function.zip_csv_lambda.arn
+output "lambda_function_arn" {
+  value = module.lambda.lambda_arn
 }
